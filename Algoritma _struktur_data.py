2d1606e7766b124b8c15cf96aa931bcc935b143f
SittiRahmaRamadani <101309159+SittiRahmaRamadani@users.noreply.github.com>
2022-03-14 08:49:13 +0800

    Create Algoritma _struktur_data.py

---
+++ Algoritma _struktur_data.py
@@ -0,0 +1,95 @@
+nilai = 2.4
+
+print (nilai)
+
+print (type(nilai))
+
+angka = 25
+
+print (angka)
+
+print (type(angka))
+
+nama = "Sitti Rahma Ramadani"
+
+print (nama)
+
+print (type(nama))
+
+angka =4<5
+
+print (angka)
+
+print (type(angka))
+
+nilai1 = 11
+
+nilai2 = 2
+
+print (nilai1 + nilai2)
+
+print (nilai1 - nilai2)
+
+print(nilai1 * nilai2)
+
+print(nilai1 / nilai2)
+
+print(nilai1 ** nilai2)
+
+print(nilai1 % nilai2)
+
+print(nilai1 // nilai2)
+
+nilai = 2
+
+nilai += 5
+
+print (nilai)
+
+nilai = 2
+
+nilai *= 5
+
+print (nilai)
+
+nilai = 2
+
+nilai -= 5
+
+print (nilai)
+
+nilai = 2
+
+nilai /= 5
+
+print (nilai)
+
+nilai = 2
+
+nilai %= 5
+
+print (nilai)
+
+nilai = 2
+
+nilai **= 5
+
+print (nilai)
+
+nilai = 2
+
+nilai //= 5
+
+print (nilai)
+
+print (1==1)
+
+print (2!=4)
+
+print (4<3)
+
+print (8>11)
+
+print (9<=12)
+
+print (10>=13)
